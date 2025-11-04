969b0fdd0f8f01f1538992d6b9bb2e0e0286c924
mina-etml <pk60ftv@eduvaud.ch>
2025-11-04 14:37:40 +0100

    L2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Boots',
+            description: 'Sturdy item of footwear covering the foot and ankle, and sometimes also the lower leg.'
+        }
+    }
+});
